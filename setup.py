a2adda50b2cd748c6fb4902b6d026d3adeedf55b
DmitriLyalikov <dlyalikov01@manhattan.edu>
2023-03-27 17:32:36 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+"""
+Run this setup.py file to install requirements for this project.
+"""
+setup(
+    name='Pendant-Drop-Tensiometry-Regressor',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'imageio',
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'sklearn',
+        'xgboost',
+        'pandas'
+    ],
+    description="Pendant Drop Tensiometry-Regressor",
+    author_email='Dlyalikov01@manhattan.edu',
+    author='Dmitri Lyalikov'
+)
